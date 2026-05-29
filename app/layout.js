a189931dd131c46bb4863c2e447a945b5070b495
Elisa <elisa@mordiefuggi.it>
2026-05-29 20:55:42 +0200

    add multilingual menu translation

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,21 @@
 import "./globals.css";
 
+export const metadata = {
+  title: "Mordi e Fuggi",
+  description: "Cucina fresca, ritmo veloce",
+  manifest: "/manifest.json",
+  icons: {
+    icon: "/icon.png",
+    apple: "/icon.png",
+  },
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="it">
-      <body>{children}</body>
+      <body>
+        {children}
+      </body>
     </html>
   );
 }
